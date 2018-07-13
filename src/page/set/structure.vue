c76e6c5cd703e9ddd1156eb7cb6f287168a14c99
hzlshen <m15277968105@163.com>
2018-07-13 14:34:12 +0800

    delete platform

--- src/page/set/structure.vue
+++ src/page/set/structure.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="structure">
-      <h3 class="head_title">组织架构</h3>
+      <h3 class="head_title">平台管理</h3>
       <template>
         <el-input
           size="mini"
@@ -10,27 +10,14 @@
       </template>
       <div>
       </div>
-      <el-row>
-        <el-col :span="6">
-          <div class="treeBox">
-            <!--<treeView class="item" :tree="treeData"></treeView>-->
-            <ul >
-              <!-- <treeView class="item" :model="treeData"></treeView> -->
-            </ul>
-          </div>
-        </el-col>
-        <el-col :span="10">6</el-col>
-      </el-row>
 
     </div>
 </template>
 
 <script>
-  // import treeView from '../../components/treeView/treeView'
   export default {
       name: 'structure',
       components: {
-        // treeView
       },
       data () {
         return {
