cefb32dc43f7056f3925088a5784a942b2a34154
hzlshen <m15277968105@163.com>
2018-07-16 00:05:48 +0800

    交易流水

--- src/page/inquiry/BankFlow.vue
+++ src/page/inquiry/BankFlow.vue
@@ -38,17 +38,17 @@
                   </el-table-column>
                   <el-table-column
                     prop="platform"
-                    label="平台"
+                    label="交易日期"
                     width="200">
                   </el-table-column>
                   <el-table-column
                     prop="orderNo"
-                    label="平台订单号"
+                    label="收入"
                     width="250">
                   </el-table-column>
                   <el-table-column
                     prop="totalAmount"
-                    label="总金额"
+                    label="支出"
                     width="80">
                   </el-table-column>
                   <el-table-column
@@ -57,12 +57,7 @@
                     width="90"
                     :formatter="formatStatus">
                   </el-table-column>
-                  <el-table-column
-                    prop="businessDate"
-                    label="业务日期"
-                    width="100"
-                    :formatter="formatTime">
-                  </el-table-column>
+
                   <el-table-column
                     prop="settleStatus"
                     label="对账状态"
@@ -123,41 +118,7 @@
                     label="订单总金额"
                     width="100">
                   </el-table-column>
-                  <el-table-column
-                    prop="commission"
-                    label="佣金"
-                    width="100">
-                  </el-table-column>
-                  <el-table-column
-                    prop="integral"
-                    label="积分"
-                    width="100">
-                  </el-table-column>
-                  <el-table-column
-                    prop="platformCoupon"
-                    label="平台优惠卷"
-                    width="110">
-                  </el-table-column>
-                  <el-table-column
-                    prop="companyCoupon"
-                    label="公司优惠卷"
-                    width="110">
-                  </el-table-column>
-                  <el-table-column
-                    prop="insurance"
-                    label="保险"
-                    width="100">
-                  </el-table-column>
-                  <el-table-column
-                    prop="freight"
-                    label="运费"
-                    width="100">
-                  </el-table-column>
-                  <el-table-column
-                    prop="sellerPayment"
-                    label="买家支付金额"
-                    width="110">
-                  </el-table-column>
+                
                 </el-table>
               </template>
             </el-tab-pane>
