cfa527c156374b0cbf51c9cdd8ef3963f5a7beab
hzlshen <m15277968105@163.com>
2019-01-08 15:39:57 +0800

    feat(): select component

--- src/components/template/hzlSelect.vue
+++ src/components/template/hzlSelect.vue
@@ -1,11 +1,45 @@
 <template>
   <div>
-    <el-select></el-select>  
+     <el-select v-model="select" :placeholder="placeholder" :size="size" @change="hanleChange">
+      <el-option
+        v-for="item in list"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value">
+      </el-option>
+    </el-select>
   </div>  
 </template>
 <script>
 export default {
-  
+  props: {
+    value: {
+      type: String,
+      default: []
+    },
+    placeholder: {
+      type: String,
+      default: '请选择...'
+    },
+    size: {
+      type: String,
+      default: 'small'
+    },
+    list: {
+      type: Array,
+      default: []
+    }
+  },
+  data() {
+    return {
+      select: ""
+    }
+  },
+  methods: {
+    hanleChange() {
+      this.$emit('input', this.select)
+    }
+  }
 }
 </script>
 <style>
