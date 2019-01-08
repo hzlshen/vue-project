97beecb4f0cbf9e2b1d256254c9856b83c5493d4
hzlshen <m15277968105@163.com>
2019-01-08 18:12:17 +0800

    Update head.vue
    调整

--- src/components/head/head.vue
+++ src/components/head/head.vue
@@ -123,10 +123,10 @@
   .lan {
     display: inline-block;
     cursor: pointer;
-    width: 20px;
-    height: 20px;
-    font-size: 14px;
-    line-height: 20px;
+    width: 22px;
+    height: 22px;
+    font-size: 12px;
+    line-height: 22px;
     text-align: center;
     background: #fff;
     border-radius: 50%;
