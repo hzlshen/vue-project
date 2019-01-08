c1e9169c17f99f5a1384043c96fb8e5b40f2dc55
hzlshen <m15277968105@163.com>
2019-01-08 15:56:56 +0800

    feat():page component

--- src/components/template/index.js
+++ src/components/template/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 
 import hzlInput from './hzlInput.vue'
 import hzlSelect from './hzlSelect.vue'
+import hzlPage from './hzlPage.vue'
 
 Vue.component('hzl-input',hzlInput);
 Vue.component('hzl-select',hzlSelect);
+Vue.component('hzl-page',hzlPage);
