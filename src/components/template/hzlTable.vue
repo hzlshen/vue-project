fcdbb77f64c37e4714ebaedfb6072f4790306610
hzlshen <m15277968105@163.com>
2019-01-09 15:54:04 +0800

    Update hzlTable.vue

--- src/components/template/hzlTable.vue
+++ src/components/template/hzlTable.vue
@@ -54,18 +54,7 @@ export default {
 
     },
     handleSelectionChange() {
-
     },
-    // formatter(row) {
-    //   console.log(row,"rwo");
-    //   return row.differenceProcessingAmount
-    //   // if(row.businessDate){
-    //   //   return moment(row.businessDate).format("YYYY-MM-DD");
-    //   // }
-    //   // if(row.status){
-    //   //    return (row.status === 'Save' && '保存') || (row.status === 'Audited' && '审核');
-    //   // }
-    // }
   }
 }
 </script>
