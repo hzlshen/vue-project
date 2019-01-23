617293914927337605f30f655d8572c419b447b5
hzlshen <m15277968105@163.com>
2019-01-23 11:04:46 +0800

    use

--- src/components/template/index.js
+++ src/components/template/index.js
@@ -4,8 +4,10 @@ import hzlInput from './hzlInput.vue'
 import hzlSelect from './hzlSelect.vue'
 import hzlPage from './hzlPage.vue'
 import hzlTable from './hzlTable.vue'
+import hzlDialog from './hzlDialog.vue'
 
 Vue.component('hzl-input',hzlInput);
 Vue.component('hzl-select',hzlSelect);
 Vue.component('hzl-page',hzlPage);
 Vue.component('hzl-table',hzlTable);
+Vue.component('hzl-dialog',hzlDialog);
