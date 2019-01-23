a51f81e2c2a9b0e1bc88df529bc61e97172d9423
hzlshen <m15277968105@163.com>
2019-01-23 10:59:51 +0800

    feat() import dialog model

--- src/components/template/hzlDialog.vue
+++ src/components/template/hzlDialog.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
-
+    <el-dialog
+      title="提示"
+      :visible.sync="dialogVisible"
+      width="30%"
+      :before-close="handleClose">
+      <span>这是一段信息</span>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -9,11 +19,13 @@ export default {
   name: "hzl-dialog",
   data() {
     return {
-
+      dialogVisible: false
     }
   },
   methods: {
-
+    handleClose() {
+      this.dialogVisible = false;
+    }
   },
   mounted() {
 
