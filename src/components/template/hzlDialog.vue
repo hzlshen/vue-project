fc85eb42246447ae9cbc21c044b3aff2ef775868
hzlshen <m15277968105@163.com>
2019-01-23 10:56:06 +0800

    Update hzlDialog.vue

---
+++ src/components/template/hzlDialog.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "hzl-dialog",
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+  mounted() {
+
+  }
+}
+</script>
+<style scoped>
+
+</style>
+
+
